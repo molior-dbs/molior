ef56b33558702c7d5196c943a05ff5fcb4ad82be
André Roth <neolynx@gmail.com>
2020-09-25 11:21:17 +0000

    projectversion: overlay api2

--- molior/api2/projectversion.py
+++ molior/api2/projectversion.py
@@ -1,7 +1,7 @@
 from ..app import app
 from ..auth import req_role
-from ..tools import ErrorResponse, OKResponse, is_name_valid
-from ..api.projectversion import projectversion_to_dict, do_clone, do_lock
+from ..tools import ErrorResponse, OKResponse
+from ..api.projectversion import projectversion_to_dict, do_clone, do_lock, do_overlay
 
 from ..model.projectversion import ProjectVersion, get_projectversion, get_projectversion_deps, get_projectversion_byname
 
@@ -187,11 +187,6 @@ async def clone_projectversion(request):
     if not projectversion:
         return ErrorResponse(400, "Projectversion not found")
 
-    if not name:
-        return ErrorResponse(400, "No valid name for the projectversion recieived")
-    if not is_name_valid(name):
-        return ErrorResponse(400, "Invalid project name!")
-
     return await do_clone(request, projectversion.id, name)
 
 
@@ -203,3 +198,16 @@ async def lock_projectversion(request):
         return ErrorResponse(400, "Projectversion not found")
 
     return do_lock(request, projectversion.id)
+
+
+@app.http_post("/api2/project/{project_id}/{projectversion_id}/overlay")
+@req_role("owner")
+async def overlay_projectversion(request):
+    params = await request.json()
+
+    name = params.get("name")
+    projectversion = get_projectversion(request)
+    if not projectversion:
+        return ErrorResponse(400, "Projectversion not found")
+
+    return await do_overlay(request, projectversion.id, name)
