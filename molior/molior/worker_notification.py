9c35dcce2551954c3a94e62234a563b5cec056ce
André Roth <andre.roth@roche.com>
2020-11-16 22:24:43 +0100

    cleanup

--- molior/molior/worker_notification.py
+++ molior/molior/worker_notification.py
@@ -52,11 +52,8 @@ class NotificationWorker:
         logger.info("terminating notification task")
 
     async def do_hooks(self, build_id):
-        method = None
-        url = None
-        skip_ssl = None
-        body = None
-
+        logger.info("do_hooks")
+        hooks = []
         with Session() as session:
             build = session.query(Build).filter(Build.id == build_id).first()
             if not build:
@@ -105,43 +102,59 @@ class NotificationWorker:
             }
 
             if not build.sourcerepository or not build.projectversion:
+                logger.warning("hook: no source repo and no projectversion")
                 return
 
             buildconfig = session.query(SouRepProVer).filter(SouRepProVer.sourcerepository_id == build.sourcerepository_id,
                                                              SouRepProVer.projectversion_id == build.projectversion_id).first()
             if not buildconfig:
+                logger.warning("hook: source repo not in projectversion")
                 return
             postbuildhooks = session.query(Hook).join(PostBuildHook).filter(
                     PostBuildHook.sourcerepositoryprojectversion_id == buildconfig.id)
             for hook in postbuildhooks:
+                method = None
+                url = None
+                skip_ssl = None
+                body = None
+
                 if not hook.enabled:
+                    logger.warning("hook: not enabled")
                     continue
 
                 if build.buildtype == "build" and not hook.notify_overall:
+                    logger.info("hook: top build not enabled")
                     continue
 
                 if build.buildtype == "source" and not hook.notify_src:
+                    logger.info("hook: src build not enabled")
                     continue
 
                 if build.buildtype == "deb" and not hook.notify_deb:
+                    logger.info("hook: deb build not enabled")
                     continue
 
                 try:
                     url = Template(hook.url).render(**args)
                 except Exception as exc:
                     logger.error("hook: error rendering URL template", url, exc)
-                    return
+                    continue
 
-                try:
-                    body = Template(hook.body).render(**args)
-                except Exception as exc:
-                    logger.error("hook: error rendering BODY template", url, exc)
-                    return
+                if hook.body:
+                    try:
+                        body = Template(hook.body).render(**args)
+                    except Exception as exc:
+                        logger.error("hook: error rendering BODY template", url, exc)
+                        continue
 
                 method = hook.method
                 skip_ssl = hook.skip_ssl
 
-        try:
-            await trigger_hook(method, url, skip_ssl=skip_ssl, body=body)
-        except Exception as exc:
-            logger.error("hook: error calling {} '{}': {}".format(method, url, exc))
+                logger.info("adding hook: %s" % url)
+                hooks.append((method, url, skip_ssl, body))
+
+        for hook in hooks:
+            try:
+                await trigger_hook(hook[0], hook[1], skip_ssl=hook[2], body=hook[3])
+            except Exception as exc:
+                logger.error("hook: error calling {} '{}': {}".format(hook[0], hook[1], exc))
