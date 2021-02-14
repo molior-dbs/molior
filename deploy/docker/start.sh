2702ed67b6be9de27b9422b8dfbde8591fac9b95
André Roth <neolynx@gmail.com>
2021-02-14 21:22:33 +0100

    docker: fix startup

--- deploy/docker/start.sh
+++ deploy/docker/start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-set -x
-
-sleep 30
 
 create-molior-keys "Molior Debsign" debsign@molior.info
 create-aptly-keys "Molior Reposign" reposign@molior.info
@@ -9,4 +6,4 @@ create-aptly-passwd molior molior-dev
 su molior -c "gpg1 --armor --export debsign@molior.info | gpg1 --import --no-default-keyring --keyring=trustedkeys.gpg"
 
 /usr/lib/molior/db-upgrade.sh
-su molior -c "/usr/bin/python3 -m molior.molior.server --host=localhost --port=8888"
+su molior -c "/usr/bin/python3 -m molior.molior.server --host=molior --port=8888"
