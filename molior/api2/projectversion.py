fc220d0e2901f94a39c69b0d9fecac55cb97e1aa
André Roth <neolynx@gmail.com>
2023-07-25 23:02:53 +0200

    allow snapshotting overlays

--- molior/api2/projectversion.py
+++ molior/api2/projectversion.py
@@ -594,8 +594,8 @@ async def snapshot_projectversion(request):
         return ErrorResponse(400, "Projectversion not found")
     if projectversion.basemirror.external_repo:
         return ErrorResponse(400, "Projectversion is based on external mirror")
-    if projectversion.projectversiontype in ['overlay', 'snapshot']:
-        return ErrorResponse(400, "Projectversion is of type overlay/snapshot")
+    if projectversion.projectversiontype == 'snapshot':
+        return ErrorResponse(400, "Cannot snapshot snapshots")
 
     if not name:
         return ErrorResponse(400, "No valid name for the projectversion recieived")
