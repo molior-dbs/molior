79c40a096cf685c260f116172f6dd68912433c1b
Dominik Schneider <dominik.schneider@accenture.com>
2023-09-20 11:50:27 +0200

    server restart: encapsuled stop/cancel commands in try-except blocks

--- molior/molior/server.py
+++ molior/molior/server.py
@@ -168,10 +168,83 @@ class MoliorServer(cirrina.Server):
 
         self.logger.info("terminating tasks")
 
+    async def terminate(self):
+
+        logger.info("terminating tasks")
+
         self.task_worker.cancel()
         self.task_backend_worker.cancel()
         self.task_aptly_worker.cancel()
         self.task_notification_worker.cancel()
+
+        try:
+            await self.task_worker
+            await self.task_backend_worker
+            await self.task_aptly_worker
+            await self.task_notification_worker
+        except asyncio.CancelledError:
+            logger.info("tasks were canceled")
+        else:
+            logger.info("tasks were completed")
+
+        try:
+            logger.info("terminating backend")
+            await self.backend.stop()
+        except asyncio.CancelledError:
+            logger.info("backend tasks were completed")
+
+        try:
+            logger.info("terminating launchy")
+            await Launchy.stop()
+        except asyncio.CancelledError:
+            logger.info("launchy tasks were completed")
+
+        logger.info("terminating app")
+        app.stop()
+
+
+@click.command()
+@click.option("--host",     default="localhost",         help="Hostname, examples: 'localhost' or '0.0.0.0'")
+@click.option("--port",     default=8888,                help="Listen port")
+@click.option("--debug",    default=False, is_flag=True, help="Enable debug")
+@click.option("--coverage", default=False, is_flag=True, help="Enable coverage testing")
+def main(host, port, debug, coverage):
+    logger.info("starting molior v%s", MOLIOR_VERSION)
+
+    if coverage:
+        logger.warning("starting coverage measurement")
+        import coverage
+        cov = coverage.Coverage(source=["molior"])
+        cov.start()
+
+    loop = asyncio.get_event_loop()
+    moliorserver = MoliorServer(loop, host, port, logger=logger, debug=debug)
+
+    def terminate(signame):
+        logger.info("received %s, terminating...", signame)
+        asyncio.run_coroutine_threadsafe(moliorserver.terminate(), loop)
+        # tasks = [task for task in asyncio.all_tasks() if task is not asyncio.tasks.current_task()]
+        # list(map(lambda task: task.cancel(), tasks))
+        # await asyncio.gather(*tasks, return_exceptions=True)
+        # try:
+        #     loop.stop()
+        # except Exception as exc:
+        #     logger.exception(exc)
+        # logger.info("event loop stopped")
+
+    for signame in ('SIGINT', 'SIGTERM'):
+        loop.add_signal_handler(getattr(signal, signame), functools.partial(terminate, signame))
+
+    moliorserver.run()  # server up and running ...
+
+    if coverage:
+        logger.warning("saving coverage measurement")
+        cov.stop()
+        cov.html_report(directory='/var/lib/molior/buildout/coverage')
+
+    logger.info("terminated")
+
+>>>>>>> server restart: encapsuled stop/cancel commands in try-except blocks
 
         try:
             await self.task_worker
