dedad7e4b3e4ff256923940880ec52c3b00e233b
André Roth <neolynx@gmail.com>
2023-11-03 15:53:12 +0000

    use file based web sessions

--- molior/app.py
+++ molior/app.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO, format='molior: %(message)s',
                     handlers=[logging.handlers.SysLogHandler(address='/dev/log')])
 logger = logging.getLogger("molior")
 
-app = cirrina.Server()
+app = cirrina.Server(session_type=cirrina.Server.SessionType.FILE, session_dir="/var/lib/molior/web-sessions/")
 app.title = "Molior REST API Documentation"
 app.description = "Documentation of the molior REST API."
 app.api_version = 1
