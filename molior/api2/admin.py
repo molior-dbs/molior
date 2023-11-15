0ec184ed431366b424e8c401fa899ee7dca28822
madeleinefr <madeleine.frohnholzer@gmail.com>
2023-11-15 10:47:39 +0100

    Feature/cleanup api

--- molior/api2/admin.py
+++ molior/api2/admin.py
@@ -1,54 +1,13 @@
-from molior import app
-from molior.auth.auth import req_role
+from ..auth import req_role
 from molior.model.metadata import MetaData
 from molior.model.projectversion import get_projectversion
 from molior.tools import OKResponse
+from ..app import app
 
 
 @app.http_put("/api2/cleanup")
-@req_role("admin")
 async def edit_cleanup(request):
-    """
-    Modify the weekly cleanup configuration
 
-    ---
-    description: Modify a project version
-    tags:
-        - ProjectVersions
-    parameters:
-        - name: project_id
-          in: path
-          required: true
-          type: string
-        - name: projectversion_id
-          in: path
-          required: true
-          type: string
-        - name: body
-          in: body
-          required: true
-          schema:
-            type: object
-            properties:
-                description:
-                    type: string
-                    example: "This version does this and that"
-                dependency_policy:
-                    type: string
-                    description: Dependency policy
-                    example: strict
-                retention_successful_builds:
-                    type: integer
-                retention_failed_builds:
-                    type: integer
-    produces:
-        - text/json
-    responses:
-        "200":
-            description: successful
-        "400":
-            description: Projectversion not found
-    """
     params = await request.json()
     cleanup_active = params.get("cleanup_active")
     cleanup_weekdays = params.get("cleanup_weekdays")
@@ -76,4 +35,4 @@ async def edit_cleanup(request):
 
     db.commit()
 
-    return OKResponse({"id": MetaData.id, "name": MetaData.name})
+    return OKResponse("Cleanup job is being configured")
