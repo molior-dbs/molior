4dbd5e6598cae0e4f516b54f84c33bbb1c5cb6c1
André Roth <neolynx@gmail.com>
2024-08-02 22:27:54 +0200

    docker build: improve build output

--- molior/backends/docker/docker.py
+++ molior/backends/docker/docker.py
@@ -86,7 +86,7 @@ class DockerBackend:
                 await enqueue_backend({"started": build_id})
 
                 await write_log_title(build_id, "Docker Build")
-                await buildlog(build_id, "Pulling build container ...\n")
+                await buildlog(build_id, "\x1b[36m\x1b[1mPulling build container ...\x1b[0m\n")
 
                 server_url = Configuration().server.get("url")
                 cfg = Configuration("/etc/molior/backend-docker.yml")
